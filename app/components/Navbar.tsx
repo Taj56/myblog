0c777b041e40aa2643d9b372a34f375f073cad16
Taj56 <64923628+Taj56@users.noreply.github.com>
2024-08-22 14:15:24 -0500

    add mode toggle button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import {ModeToggle} from "@/app/components/ModeToggle";
 
 const Navbar = () => {
     return (
         <nav className={"w-full flex relative justify-between items-center p-5 max-w-2xl border mx-auto"}>
-            <Link href="/" className={"font-bold text-3xl"}>Tajay's <span className={"text-blue-500"}>Comic Thoughts</span></Link>
+            <Link href="/" className={"font-bold text-3xl"}>Tajay's <span className={"text-primary"}>Comic Thoughts</span></Link>
+            <ModeToggle />
         </nav>
     )
 }
