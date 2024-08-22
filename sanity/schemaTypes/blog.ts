b30a6bb43d9ad45c94cfdb098b080b8ce91354af
Taj56 <64923628+Taj56@users.noreply.github.com>
2024-08-22 13:23:22 -0500

    add options to slug
    add options field to slug so inside sanity studio we will have to option to generate a slug name based on the title

--- sanity/schemaTypes/blog.ts
+++ sanity/schemaTypes/blog.ts
@@ -12,6 +12,9 @@ export default {
             name: 'slug',
             type: 'slug',
             title: 'Slug of the blog',
+            options: {
+                source: 'title',
+            }
         },
         {
           name: 'titleImage',
