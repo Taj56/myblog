0aa9e00592d090cc84bc98a984723fa27596d436
Taj56 <64923628+Taj56@users.noreply.github.com>
2024-08-22 15:19:59 -0500

    add blog cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import {client, urlFor} from "@/app/lib/sanity";
 import {blogCard} from "@/app/lib/interface";
-import {Card} from "@/components/ui/card";
+import {Card, CardContent} from "@/components/ui/card";
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
 
 const getData = async () =>{
     const query = `
@@ -24,10 +26,21 @@ const Home = async () => {
 
   return (
     <>
-      <div className={"grid grid-cols-1 m:grid-cols-2 mt-5 "}>
+      <div className={"grid grid-cols-1 md:grid-cols-2 mt-5 gap-5"}>
           {data.map((post, idx)=>(
             <Card key={idx}>
-                <Image src={urlFor(post.titleImage).url()} alt={'Image'} width={300} height={300} />
+                <Image src={urlFor(post.titleImage).url()} alt={'Image'}
+                       width={500} height={500}
+                       className={"rounded-t-lg h-[200px] object-cover object-center"}
+                />
+                <CardContent className={"mt-5"}>
+                    <h3 className={"text-xl line-clamp-2 font-bold"}>{post.title}</h3>
+
+                    <p className={"line-clamp-2 mt-1 text-sm text-gray-500"}>{post.smallDescription}</p>
+                    <Button asChild={true} className={"w-full mt-7"}>
+                        <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+                    </Button>
+                </CardContent>
             </Card>
           ))}
       </div>
