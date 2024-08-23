547895d7c3475cf2b5d2c5b3cc895d047017659b
Taj56 <64923628+Taj56@users.noreply.github.com>
2024-08-22 19:27:33 -0500

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import {ModeToggle} from "@/app/components/ModeToggle";
 const Navbar = () => {
     return (
         <nav className={"w-full flex relative justify-between items-center p-5 max-w-2xl border mx-auto"}>
-            <Link href="/" className={"font-bold text-3xl"}>Tajay's <span className={"text-primary"}>Comic Thoughts</span></Link>
+            <Link href="/" className={"font-bold text-3xl"}>Tajay&apos;s <span className={"text-primary"}>Comic Thoughts</span></Link>
             <ModeToggle />
         </nav>
     )
